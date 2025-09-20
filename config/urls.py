65fcebd3393ebf6dd69798168258df88892cfe82
Abdullox <your-email@example.com>
2025-09-20 17:18:30 +0300

    Abdullox

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('main.urls')),  # core - app nomi
+    path("admin/", admin.site.urls),
+    path("api/", include("main.urls")),  # asosiy app
+
+    # ✅ Swagger va ReDoc
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
